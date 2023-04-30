e9d8d72203ddd65d7bb1c6bd44921042261dc16c
VIWARR <dm.pavlyuchenko@gmail.com>
2023-04-30 18:02:53 +0300

    page_count

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import fake_useragent
 import time
 import json
 
-url_vanille = 'https://vanille.by/otlivant-duhi-na-razliv?page=1'
+url_vanille = 'https://vanille.by/otlivant-duhi-na-razliv?page=35'
 
 def get_links():
     ua = fake_useragent.UserAgent()
@@ -12,7 +12,16 @@ def get_links():
         url=url_vanille,
         headers={'user-agent': ua.random}
     )
-    print(data.content)
+
+    if data.status_code != 200:
+        return
+
+    soup = BeautifulSoup(data.content, 'lxml')
+    try:
+        page_count = int(soup.find('div', {'class': 'content__flex-container'}).find_all('li')[-1].find('span').text)
+    except:
+        return
+    print(page_count)
 
 
 if __name__ == "__main__":
